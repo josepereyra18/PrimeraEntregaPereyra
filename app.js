195341759cfab589ef00023462c342674f3b3263
Josefina Pereyra <j08pereyra@gmail.com>
2023-11-15 17:14:07 -0300

    Segunda Entrega- reentrega

---
+++ app.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const ProductManager = require('./Clase0'); // Asegúrate de ajustar la ruta para que apunte al archivo correcto
+
+// const app = express();
+// const port = 8080;
+
+// const productManager = new ProductManager('./products.json'); // Asume que los productos se guardan en 'products.json'
+
+// app.get('/', (req, res) => {
+//     res.json(productManager.getProducts());
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
